8e4e00dd2ddc38de12fb36a2a5ac5175da28d312
Ankur Gupta <ankur@ankurgupta.net>
2020-04-19 13:41:18 +0530

    First commit.

---
+++ src/test/java/com/braindevoiler/security/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braindevoiler.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
